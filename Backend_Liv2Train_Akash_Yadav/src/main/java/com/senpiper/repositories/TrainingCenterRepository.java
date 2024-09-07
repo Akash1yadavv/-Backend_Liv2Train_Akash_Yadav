f0f08badca4512346214544cd47962557e4940ac
Akash Yadav <akashyadav55999@gmail.com>
2024-09-08 00:52:57 +0530

    Shorting and searching completed

--- Backend_Liv2Train_Akash_Yadav/src/main/java/com/senpiper/repositories/TrainingCenterRepository.java
+++ Backend_Liv2Train_Akash_Yadav/src/main/java/com/senpiper/repositories/TrainingCenterRepository.java
@@ -1,5 +1,6 @@
 package com.senpiper.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,10 @@ import com.senpiper.model.TrainingCenter;
 @Repository
 public interface TrainingCenterRepository extends JpaRepository<TrainingCenter, Long> {
 	Optional<TrainingCenter> findByCenterCode(String centerCode);
+    // Filter by Center Name (Optional)
+    List<TrainingCenter> findByCenterNameContaining(String centerName);
+    
+    // Filter by City (Optional)
+    List<TrainingCenter> findByAddress_City(String city);
+
 }
