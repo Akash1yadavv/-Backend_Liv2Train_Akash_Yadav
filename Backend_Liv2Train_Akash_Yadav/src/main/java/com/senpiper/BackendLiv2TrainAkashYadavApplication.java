ca9cc77f56692b4b03d819655b8e7b9fd465444d
Akash Yadav <akashyadav55999@gmail.com>
2024-09-07 22:48:33 +0530

    Project Initiated

---
+++ Backend_Liv2Train_Akash_Yadav/src/main/java/com/senpiper/BackendLiv2TrainAkashYadavApplication.java
@@ -0,0 +1,13 @@
+package com.senpiper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLiv2TrainAkashYadavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLiv2TrainAkashYadavApplication.class, args);
+	}
+
+}
---
+++ Backend_Liv2Train_Akash_Yadav/src/test/java/com/senpiper/BackendLiv2TrainAkashYadavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senpiper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLiv2TrainAkashYadavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
